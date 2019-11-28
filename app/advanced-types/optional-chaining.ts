6a13fbfe0646bb1c3b89fab5b84e80c5e460f78e
Irina Antonets <i.antonets@spheremall.com>
2019-11-28 17:41:19 +0200

    Comment out extra console.log

--- app/advanced-types/optional-chaining.ts
+++ app/advanced-types/optional-chaining.ts
@@ -6,7 +6,7 @@ const fetchedData = {
 /* if fetched data is received from a server and we cannot
    know for sure if job prop and title prop will be there for sure
    so basically we are saying TS - check if this exists and then acces it */
-console.log(fetchedData?.job?.title);
+// console.log(fetchedData?.job?.title);
 
 
 /* You can have a situation - you want to set DEFAULT value only when 
